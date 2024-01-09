15cb48a593cfea2eeb876566d93e4adf59fb43f6
yogeshkumarhcl <155983974+yogeshkumarhcl@users.noreply.github.com>
2024-01-09 17:23:21 +0530

    Update hpcapi.py

--- hpcapi.py
+++ hpcapi.py
@@ -1,1 +1,25 @@
 # need to add code here
+
+import flask
+from flask_cors import CORS
+import json
+
+# Initializing flask app
+app = flask.Flask(__name__)
+# Adding cors to flask
+CORS(app)
+
+@app.route("/demo", methods=['GET'])
+def get_demo():
+    return "This is a demo api"
+
+@app.route("/name", methods=['GET','POST'])
+def get_demo_name():
+    data = flask.request.data
+    body = json.loads(data)
+    name = body["name"]
+    return "Hello, I am {}!".format(name)
+
+# Running the api
+if __name__ == '__main__':
+    app.run()
